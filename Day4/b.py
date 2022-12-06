adfda184f942f245548a6a6e4a435a0932d3ef19
Spyabo <reesewright02@protonmail.com>
2022-12-06 03:08:57 +0000

    comment

--- Day4/b.py
+++ Day4/b.py
@@ -2,14 +2,14 @@ txt = open("/home/spy/AoC2022/Day4/input.txt", "r")
 pairs = [i for i in txt.read().strip().split("\n")]
 
 def contained(a, b):
-    # Return True if a fully contains b, False otherwise
+    # Return True if the ranges a and b overlap, False otherwise
     start_a, end_a = map(int, a.split('-'))
     start_b, end_b = map(int, b.split('-'))
     
     return (start_a <= start_b <= end_a) or (start_b <= start_a <= end_b)
 
 def num_overlapping_pairs(pairs):
-    # Iterate over each pair and check if one fully contains the other
+    # Iterate over each pair and check if they overlap
     count = 0
     
     for pair in pairs:
