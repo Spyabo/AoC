99deb0a4886dd3acb6bc63f9f13c2c45aa10c4fd
Spyabo <reesewright02@protonmail.com>
2023-12-02 12:53:27 +0000

    p1

--- 2023/Day2/a.py
+++ 2023/Day2/a.py
@@ -1,2 +1,34 @@
-with open("./2023/Day2/testinput.txt", "r") as f:
+with open("./2023/Day2/input.txt", "r") as f:
     lines = f.read().splitlines()
+
+lookup = {
+    "red": 12,
+    "green": 13,
+    "blue": 14
+}
+
+total = 0
+games = 0
+  
+for i, line in enumerate(lines):
+    game_id = i + 1
+    turns = line.split(":")[1].split(";")
+    total += game_id
+   
+    for turn in turns:
+        flag = True
+        move = turn.split(" ")[1:]
+        for i in range(0, len(move), 2):
+            if flag == False:
+                break
+            amount = int(move[i])
+            colour = move[i+1].replace(",", "")
+            if amount > lookup[colour]:
+                total -= game_id
+                flag = False
+        if flag == False:
+            break
+
+    
+print(total)
+            
