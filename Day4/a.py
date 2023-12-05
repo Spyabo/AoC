aadae39868e354c7e6152fc3f5fa56efe5398a7a
Spyabo <reesewright02@protonmail.com>
2023-12-05 20:22:08 +0000

    Day4 start

---
+++ Day4/a.py
@@ -0,0 +1,9 @@
+with open("./2023/Day4/testinput.txt", "r") as f:
+    lines = f.read().splitlines()
+
+total = 0
+
+
+    
+print(total)
+            
---
+++ Day4/b.py
@@ -0,0 +1,6 @@
+with open("./2023/Day2/input.txt", "r") as f:
+    lines = f.read().splitlines()
+
+total = 0
+
+print(total)
