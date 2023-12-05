aa271a00448c1c2a3a49f72ef427fac17bec6480
Spyabo <reesewright02@protonmail.com>
2023-12-05 21:43:26 +0000

    Finished :)

--- 2023/Day4/b.py
+++ 2023/Day4/b.py
@@ -2,15 +2,20 @@ with open("./2023/Day4/input.txt", "r") as f:
     lines = f.read().splitlines()
 
 import re
+from collections import defaultdict
 
-total = 0
+copies = defaultdict(list)
 
 for i, line in enumerate(lines):
+    if i not in copies:
+        copies[i] = 1
+
     left, right = line.split(":")[1].split("|")
     winners, mine = re.findall(r"\d+", left), re.findall(r"\d+", right)
     match = set(winners) & set(mine)
 
     if len(match) > 0:
-        total += 2 ** len(match) // 2
+        for j in range(i + 1, i + len(match) + 1):
+            copies[j] = copies.get(j, 1) + copies[i]
 
-print(total)
+print(sum(copies.values()))
