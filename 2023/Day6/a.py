df13e933be9c157be6f92eea6d95fc7fe228da04
Spyabo <reesewright02@protonmail.com>
2023-12-06 21:06:39 +0000

    part 1!!!

--- 2023/Day6/a.py
+++ 2023/Day6/a.py
@@ -1,9 +1,26 @@
-with open("./2023/Day2/input.txt", "r") as f:
+with open("./2023/Day6/input.txt", "r") as f:
     lines = f.read().splitlines()
 
-total = 0
+import re
 
+amount = []
+ans = 1
 
-    
-print(total)
-            
+times = re.findall(r"\d+", lines[0])
+distances = re.findall(r"\d+", lines[1])
+
+for time, distance in zip(times, distances):
+    speed = 0
+    total = 0
+    for i in range(int(time)):
+        time_left = int(time) - i
+        travel = speed * time_left
+        if travel > int(distance):
+            total += 1
+
+        speed += 1
+    amount.append(total)
+
+for num in amount:
+    ans *= num
+print(ans)
