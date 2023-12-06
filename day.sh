de2103143e3ff51b613777ec95aa3c08848ffe72
Spyabo <reesewright02@protonmail.com>
2023-12-06 22:35:26 +0000

    Nice bash script to create new days

---
+++ day.sh
@@ -0,0 +1,13 @@
+day=$1
+mkdir -p "./2023/Day$day"
+touch "./2023/Day$day/input.txt"
+touch "./2023/Day$day/testinput.txt"
+touch "./2023/Day$day/a.py"
+touch "./2023/Day$day/b.py"
+
+# Python code to be added
+python_code="with open('./2023/Day$day/testinput.txt', 'r') as f:\n    lines = f.read().splitlines()"
+
+# Write Python code to a.py and b.py
+echo "$python_code" > "./2023/Day$day/a.py"
+echo "$python_code" > "./2023/Day$day/b.py"
