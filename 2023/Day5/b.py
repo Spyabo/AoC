980b4346624e1e7cc0cbb227ca7c474441d970f2
Spyabo <reesewright02@protonmail.com>
2023-12-08 22:19:45 +0000

    need to optimise

--- 2023/Day5/b.py
+++ 2023/Day5/b.py
@@ -1,2 +1,22 @@
-with open('./2023/Day5/testinput.txt', 'r') as f:
-    lines = f.read().splitlines()
+with open("./2023/Day5/input.txt", "r") as f:
+    lines = list(filter(lambda x: x != "", f.read().split("\n\n")))
+
+import re
+
+seeds = list(map(int, re.findall(r"\d+", lines[0])))
+seeds = list(range(seeds[0], seeds[0] + seeds[1]))
+maps = lines[1:]
+ans = []
+
+for seed in seeds:
+    for m in maps:
+        values = re.findall(r"\d+", m)
+        for i in range(0, len(values), 3):
+            drs, srs, rl = int(values[i]), int(values[i + 1]), int(values[i + 2])
+            if srs <= seed < srs + rl:
+                seed = drs + (seed - srs)
+                break
+
+    ans.append(seed)
+
+print(min(ans))
