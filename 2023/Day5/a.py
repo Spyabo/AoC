8a41be96bf0f5a508c8011f4e293221ea3205b18
Spyabo <reesewright02@protonmail.com>
2023-12-08 00:56:31 +0000

    part 1 :)

--- 2023/Day5/a.py
+++ 2023/Day5/a.py
@@ -1,22 +1,21 @@
-with open("./2023/Day5/input.txt", "r") as f:
-    lines = list(filter(lambda x: x != "", f.read().splitlines()))
+with open("./2023/Day5/testinput.txt", "r") as f:
+    lines = list(filter(lambda x: x != "", f.read().split("\n\n")))
 
 import re
 
 seeds = re.findall(r"\d+", lines[0])
+maps = lines[1:]
 ans = []
 
 for seed in seeds:
     seed = int(seed)
-    for i, line in enumerate(lines[1:]):
-        values = re.findall(r"\d+", line)
-        if values == []:
-            continue
-        drs, srs, rl = int(values[0]), int(values[1]), int(values[2])
-        if seed in range(srs, srs + rl):
-            # sources = [x for x in range(srs, srs + rl)]
-            # destinations = [x for x in range(drs, drs + rl)]
-            seed = drs + (seed - srs)
+    for m in maps:
+        values = re.findall(r"\d+", m)
+        for i in range(0, len(values), 3):
+            drs, srs, rl = int(values[i]), int(values[i + 1]), int(values[i + 2])
+            if srs <= seed < srs + rl:
+                seed = drs + (seed - srs)
+                break
 
     ans.append(seed)
 
