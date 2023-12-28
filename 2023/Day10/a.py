40364a54403d4ce579e244f1ab762cac9b36b9b5
Spyabo <reesewright02@protonmail.com>
2023-12-28 23:02:37 +0000

    part 1 with custom algo :)

--- 2023/Day10/a.py
+++ 2023/Day10/a.py
@@ -1,6 +1,6 @@
-with open("./2023/Day10/testinput.txt", "r") as f:
+with open("./2023/Day10/input.txt", "r") as f:
     lines: str = f.read().splitlines()
-# 4687
+
 # | is a vertical pipe connecting north and south.
 # - is a horizontal pipe connecting east and west.
 # L is a 90-degree bend connecting north and east.
@@ -8,40 +8,75 @@ with open("./2023/Day10/testinput.txt", "r") as f:
 # 7 is a 90-degree bend connecting south and west.
 # F is a 90-degree bend connecting south and east.
 
-def find_s(grid):
+
+def find_start(grid):
     for i, line in enumerate(grid):
         if "S" in line:
             return line.index("S"), i
 
 
-def find_starts(grid, x, y):
-    starts = []
+def find_paths(grid, x, y):
+    paths = []
+    #  N, E, S, W
+    directions = [(0, -1), (1, 0), (0, 1), (-1, 0)]
 
-    for i, direction in enumerate([[0, 1], [1, 0], [0, -1], [-1, 0]]):
+    for i, direction in enumerate(directions):
         nx, ny = x + direction[0], y + direction[1]
-        
+
         if nx < 0 or nx >= len(grid[0]) or ny < 0 or ny >= len(grid):
             continue
-        
+
         if i == 0 and grid[ny][nx] in ["|", "7", "F"]:
-            starts.append((nx, ny, grid[ny][nx]))
+            paths.append((nx, ny, grid[ny][nx], directions[i]))
         if i == 1 and grid[ny][nx] in ["-", "J", "7"]:
-            starts.append((nx, ny, grid[ny][nx]))
+            paths.append((nx, ny, grid[ny][nx], directions[i]))
         if i == 2 and grid[ny][nx] in ["|", "L", "J"]:
-            starts.append((nx, ny, grid[ny][nx]))
+            paths.append((nx, ny, grid[ny][nx], directions[i]))
         if i == 3 and grid[ny][nx] in ["-", "L", "F"]:
-            starts.append((nx, ny, grid[ny][nx]))
-    
-    return starts
+            paths.append((nx, ny, grid[ny][nx], directions[i]))
 
-def move(grid, x, y, direction, pipe):
+    return paths
 
-    
+
+def solve(grid, x, y, paths):
+    past = [None for _ in range(len(paths))]
+    distance = 1
+    pipe_directions = {
+        "|": lambda d: d,
+        "-": lambda d: d,
+        "L": lambda d: (1, 0) if d == (0, 1) else (0, -1),
+        "J": lambda d: (-1, 0) if d == (0, 1) else (0, -1),
+        "7": lambda d: (-1, 0) if d == (0, -1) else (0, 1),
+        "F": lambda d: (1, 0) if d == (0, -1) else (0, 1),
+    }
+
+    while (x, y) not in past:
+        distance += 1
+
+        for i, path in enumerate(paths):
+            pipe = path[2]
+            cur_x, cur_y = path[0], path[1]
+            direction = path[3]
+            change = pipe_directions[pipe](direction)
+            nx, ny = cur_x + change[0], cur_y + change[1]
+            # print(direction, (nx, ny, grid[ny][nx], change), past)
+
+            if (nx, ny) in past:
+                return distance
+
+            past[i] = (nx, ny)
+            paths[i] = (nx, ny, grid[ny][nx], change)
+
+    return distance
+
+
 def main():
     grid = [list(line) for line in lines]
-    x, y = find_s(grid)
+    x, y = find_start(grid)
+    paths = find_paths(grid, x, y)
+    ans = solve(grid, x, y, paths)
 
-    while 
-        
+    print(ans)
+
 
 main()
