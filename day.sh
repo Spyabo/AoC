c5aaed62f1587f2031d1b901652621896424a607
Spyabo <reesewright02@protonmail.com>
2024-11-30 16:37:29 +0000

    Updated script to get the input

--- day.sh
+++ day.sh
@@ -1,13 +1,19 @@
-day=$1
-mkdir -p "./2023/Day$day"
-touch "./2023/Day$day/input.txt"
-touch "./2023/Day$day/testinput.txt"
-touch "./2023/Day$day/a.py"
-touch "./2023/Day$day/b.py"
+day=$(printf "%02d" "$1")
+year=${2:-$(date +'%Y')}
+cookie=53616c7465645f5ff37ddcacaa50fb80ce82ce5c37ca9c2daf4d669d3c742c9057217627286cd4e09d57a7244b95e1c7191f9df342a33b5bb2169ec89c2ae7d3
+
+mkdir -p "./$year/Day$day"
+touch "./$year/Day$day/input.txt"
+touch "./$year/Day$day/testinput.txt"
+touch "./$year/Day$day/a.py"
+touch "./$year/Day$day/b.py"
+
+# Download input
+curl -s -H "Cookie: session=$cookie" "https://adventofcode.com/$year/day/${day#0}/input" > "./$year/Day$day/input.txt"
 
 # Python code to be added
-python_code="with open('./2023/Day$day/testinput.txt', 'r') as f:\n    lines: str = f.read().splitlines()"
+python_code="with open('./$year/Day$day/testinput.txt', 'r') as f:\n    lines: str = f.read().splitlines()"
 
 # Write Python code to a.py and b.py
-echo "$python_code" > "./2023/Day$day/a.py"
-echo "$python_code" > "./2023/Day$day/b.py"
+echo "$python_code" > "./$year/Day$day/a.py"
+echo "$python_code" > "./$year/Day$day/b.py"
