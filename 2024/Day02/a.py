a8c45a838e28519bed138eee094d8663f2759350
Spyabo <reesewright02@protonmail.com>
2024-12-03 01:41:27 +0000

    day02

--- 2024/Day02/a.py
+++ 2024/Day02/a.py
@@ -1,4 +1,4 @@
-with open('./input.txt', 'r') as f:
+with open('./2024/Day02/input.txt', 'r') as f:
     lines: str = f.read().splitlines()
 
 safe = 0
@@ -7,12 +7,20 @@ for line in lines:
     levels = list(map(int, line.split()))
 
     cur = levels[0]
-
+    if levels[1] > cur:
+        increasing = True
+    else:
+        increasing = False
+    
     for level in levels[1:]:
-        if level > cur:
-            increasing = True
-        else:
-            increasing = False
-
+        if abs(level - cur) > 3 or level == cur:
+            break
         
+        if increasing and level < cur or not increasing and level > cur:
+            break
 
+        cur = level
+    else:
+        safe += 1
+        
+print(safe)
--- 2024/Day02/b.py
+++ 2024/Day02/b.py
@@ -1,1 +1,15 @@
-with open('./2024/Day02/testinput.txt', 'r') as f:\n    lines: str = f.read().splitlines()
+with open('./2024/Day02/input.txt', 'r') as f:
+    lines: str = f.read().splitlines()
+
+safe = 0
+
+def is_safe(levels):
+    diffs = [x - y for x, y in zip(levels, levels[1:])]
+    return all(1 <= x <= 3 for x in diffs) or all(-1 >= x >= -3 for x in diffs)
+
+for line in lines:
+    levels = list(map(int, line.split()))
+    if any(is_safe(levels[:index] + levels[index + 1:]) for index in range(len(levels))):
+        safe += 1
+        
+print(safe)
