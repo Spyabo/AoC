a3289f924ef5f090657b63d8198ccd4131898285
spyabo <reesewright02@protonmail.com>
2024-12-03 15:05:41 +0000

    Day03 a

---
+++ 2024/Day03/a.py
@@ -0,0 +1,16 @@
+with open('./2024/Day03/input.txt', 'r') as f:
+    lines: str = f.read().splitlines()
+
+import re
+from functools import reduce
+
+res = 0
+
+for line in lines:
+    # Find strings in the format "mul(x, y)"
+    muls = re.findall(r"mul\((\d+),(\d+)\)", line)
+    
+    for mul in muls:
+        res += int(mul[0]) * int(mul[1])
+
+print(res)
---
+++ 2024/Day03/b.py
@@ -0,0 +1,2 @@
+with open('./2024/Day03/testinput.txt', 'r') as f:
+    lines: str = f.read().splitlines()
