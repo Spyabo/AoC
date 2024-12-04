251157758df7e7f99f84702b0ecc2c877ad7970a
Spyabo <reesewright02@protonmail.com>
2024-12-04 16:31:28 +0000

    15/18

--- 2024/Day04/a.py
+++ 2024/Day04/a.py
@@ -1,6 +1,11 @@
 with open('./2024/Day04/testinput.txt', 'r') as f:
     lines: str = f.read().splitlines()
 
+def direction_print(direction):
+    lookup = {(0, -1): "↑",(1, -1): "↗",(1, 0): "→",(1, 1): "↘",(0, 1): "↓",(-1, 1): "↙",(-1, 0): "←",(-1, -1): "↖",}
+    
+    return lookup[direction]
+
 def find_starts(grid):
     starts = []
     
@@ -12,24 +17,34 @@ def find_starts(grid):
 
 # DFS from the X to check all directions to complete the word "XMAS"
 def valid_xmas(x, y, grid):
-    # clockwise
-    checks = [(0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1), (-1, 0), (-1, 1)]
+    # clockwise starting at N
+    checks = [(0, -1), (1, -1), (1, 0), (1, 1), (0, 1), (-1, 1), (-1, 0), (-1, -1)]
+    cur_total = 0
     
     for check in checks:
+        print(f"Checking X at {x}, {y} in direction {direction_print(check)}")
         nx, ny =  x + check[0], y + check[1]
         chars = ["M", "A", "S"]
         i = 0
-        total = 0
         
-        while nx < 0 or nx >= len(grid[0]) or ny < 0 or ny >= len(grid):
-            if grid[nx][ny] == chars[i]:
+        while True:
+            if nx < 0 or nx == len(grid) or ny < 0 or ny >= len(grid):
+                break
+            
+            if grid[ny][nx] == chars[i]:
+                print("Found", chars[i], "at", nx, ny)
                 nx, ny = nx + check[0], ny + check[1]
                 i += 1
+            else:
+                nx, ny = nx + check[0], ny + check[1]
+                continue
             
             if i == len(chars):
-                total += 1
+                cur_total += 1
+                print("Found XMAS current total:", cur_total)
+                break
             
-        return total
+    return cur_total
             
     # for i in range(range_start, range_end):
 
@@ -39,7 +54,7 @@ def calculate_total(lines):
     starts = find_starts(grid)
 
     for start in starts:
-        total += valid_xmas(start[1], start[0], grid)
+        total += valid_xmas(start[0], start[1], grid)
 
 
     return total
