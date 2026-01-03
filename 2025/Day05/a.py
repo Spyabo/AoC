f658f5a5c41330d832f4eeca79563fbef13fa449
RW <rw@RWs-MacBook-Air.local>
2026-01-03 13:10:21 +0000

    Day 5 on highspeed train

--- 2025/Day05/a.py
+++ 2025/Day05/a.py
@@ -1,2 +1,23 @@
-with open('./2025/Day05/testinput.txt', 'r') as f:
+with open('./2025/Day05/input.txt', 'r') as f:
     lines: str = f.read().splitlines()
+    fresh_ranges = lines[:lines.index('')]
+    ingredients = lines[lines.index('')+1:]
+
+print(lines, fresh_ranges, ingredients)
+
+ans = 0
+used_ids = set()
+
+for item in ingredients:
+    for id_range in fresh_ranges:
+        item = int(item)
+        start, stop = int(id_range.split('-')[0]), int(id_range.split('-')[1])
+
+        if start <= item <= stop:
+            print(item, id_range)
+            if item not in used_ids:
+                ans += 1
+            used_ids.add(item)
+            
+
+print(ans)
--- 2025/Day05/b.py
+++ 2025/Day05/b.py
@@ -1,2 +1,33 @@
-with open('./2025/Day05/testinput.txt', 'r') as f:
+with open('./2025/Day05/input.txt', 'r') as f:
     lines: str = f.read().splitlines()
+    fresh_ranges = lines[:lines.index('')]
+
+print(fresh_ranges)
+
+ans = 0
+joined = [fresh_ranges[0]]
+
+for id_range in fresh_ranges[1:]:
+    start, stop = int(id_range.split('-')[0]), int(id_range.split('-')[1])
+
+    new_start, new_stop = start, stop
+    i = 0
+    while i < len(joined):
+        sta, sto = int(joined[i].split('-')[0]), int(joined[i].split('-')[1])
+
+        if new_stop < sta or sto < new_start:
+            i += 1
+            continue
+
+        new_start = min(new_start, sta)
+        new_stop = max(new_stop, sto)
+        joined.pop(i)
+
+    joined.append(f"{new_start}-{new_stop}")
+                
+
+for rg in joined:
+    start, stop = rg.split('-')
+    ans += int(stop) - int(start) + 1
+
+print(ans)
